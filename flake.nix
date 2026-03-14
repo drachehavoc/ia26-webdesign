96eeb893255908614f3c54d245bbbf20745835b7
niel <niel@localhost>
2026-03-13 21:11:45 -0300

    ...

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Aula Dev - Web Dev";
+
+  inputs = {
+    nixpkgs.url     = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux"] (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+    in rec {
+      devShell = pkgs.mkShell {
+
+        buildInputs = with pkgs; [
+          git
+          bun
+          unzip
+          bashInteractive # ISSO MANTEM O TERMINAL DO VSCODE OK
+        ];
+
+        shellHook = ''
+          export GIT_AUTHOR_NAME="$USER"
+          export GIT_COMMITTER_NAME="$USER"
+          export GIT_AUTHOR_EMAIL="$USER@localhost"
+          export GIT_COMMITTER_EMAIL="$USER@localhost"
+          
+          export PS1="(ia26-webdesign) $ "
+        '';
+      };
+    });
+}
